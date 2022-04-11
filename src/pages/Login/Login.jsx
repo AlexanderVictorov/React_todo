6132ddc62bd1dfe76d57c94d815589103358bd1b
AlexanderVictorov <0676171781.a@gmail.com>
2022-04-11 14:20:03 +0300

    loginValidation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
 import {
@@ -10,6 +10,7 @@ import Animation from '../../components/Animation/animation';
 import ROUTE_LINKS from '../../components/MyRouters/routeLink';
 
 const StyledBox = styled(Box)`
+  padding-bottom: 15px;
   display: flex;
   justify-content: center;
   background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
@@ -17,23 +18,88 @@ const StyledBox = styled(Box)`
   top: 25%;
   left: 0;
   width: 100%;
-  height: 400px;
+  height: auto;
   overflow: hidden;
 `;
 
 function Login() {
   const { setIsAuth } = useContext(AuthContext);
   const navigate = useNavigate();
+  const [userNameDirty, setUserNameDirty] = useState(false);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [userNameError, setUserNameError] = useState('Enter values for the username field');
+  const [emailError, setEmailError] = useState('Enter values for the email field');
+  const [passwordError, setPasswordError] = useState('Enter values for the password field');
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (userNameError || emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [userNameError, emailError, passwordError]);
   const [myLogin, setMyLogin] = useState({
     username: '',
     email: '',
     password: '',
   });
-  const onChange = (event) => {
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'username':
+        setUserNameDirty(true);
+        break;
+      case 'email':
+        setEmailDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+      default:
+        console.log('lol');
+    }
+  };
+
+  const emailHandler = (e) => {
     setMyLogin({
       ...myLogin,
-      [event.target.name]: event.target.value,
+      [e.target.name]: e.target.value,
     });
+    const re = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError('Not correct email');
+    } else {
+      setEmailError('');
+    }
+  };
+
+  const passwordHandler = (e) => {
+    console.log(e.target.value);
+    setMyLogin({
+      ...myLogin,
+      [e.target.name]: e.target.value,
+    });
+    if (e.target.value.length < 3) {
+      console.log('password');
+      setPasswordError('password must be longer than 3 characters');
+      if (!e.target.value) {
+        setPasswordError('Enter values for the password field');
+      } else {
+        setPasswordError('');
+      }
+    }
+  };
+  const userNameHandler = (e) => {
+    setMyLogin({
+      ...myLogin,
+      [e.target.name]: e.target.value,
+    });
+    if (!e.target.value) {
+      setUserNameError('Enter values for the username field');
+    } else {
+      setUserNameError('');
+    }
   };
   const login = async (e) => {
     e.preventDefault();
@@ -69,38 +135,50 @@ function Login() {
         <Box sx={{ marginLeft: '35px', position: 'relative', zIndex: '2' }} component='form' onSubmit={login}>
           <Box
             sx={{
-              '& > :not(style)': { m: 1, width: '25ch', display: 'flex' },
+              '& > :not(style)': { m: 1, width: '26ch', display: 'flex' },
             }}
             noValidate
             autoComplete='off'
           >
+            {(userNameDirty && userNameError)
+              && <Typography sx={{ fontSize: '12px' }} color='error'>{userNameError}</Typography>}
             <TextField
               label='Username'
               variant='outlined'
               type='text'
               name='username'
               value={myLogin.username}
-              onChange={onChange}
+              onChange={userNameHandler}
+              onBlur={blurHandler}
             />
+            {(emailDirty && emailError)
+              && <Typography sx={{ fontSize: '12px' }} color='error'>{emailError}</Typography>}
             <TextField
               label='Email'
               variant='outlined'
               type='text'
               name='email'
               value={myLogin.email}
-              onChange={onChange}
+              onChange={emailHandler}
+              onBlur={blurHandler}
             />
+            {(passwordDirty && passwordError)
+              && <Typography sx={{ fontSize: '12px' }} color='error'>{passwordError}</Typography>}
             <TextField
               label='Password'
               variant='outlined'
               type='password'
               name='password'
               value={myLogin.password}
-              onChange={onChange}
+              onChange={passwordHandler}
+              onBlur={blurHandler}
             />
           </Box>
           <Stack spacing={2} direction='row'>
-            <Button sx={{ marginLeft: '9px', width: '200px' }} variant='contained' type='submit'>Log In</Button>
+            <Button disabled={!formValid} sx={{ marginLeft: '9px', width: '207px' }} variant='contained' type='submit'>
+              Log
+              In
+            </Button>
           </Stack>
           <Typography sx={{ marginTop: '10px' }} variant='body1' className='message'>
             Not registered?
