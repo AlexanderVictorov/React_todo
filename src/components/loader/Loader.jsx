955b1b4bcabef5780758e6dd4a48387afe9d0e29
AlexanderVictorov <0676171781.a@gmail.com>
2022-04-14 09:27:34 +0300

    addLoader

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Box } from '@mui/material';
+import { useEffect } from 'react';
+
+function Loader() {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+  useEffect(() => {
+    handleToggle();
+  }, [open]);
+  return (
+    <Box>
+      {/* <Button onClick={handleToggle}>Show backdrop</Button> */}
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color='inherit' />
+      </Backdrop>
+    </Box>
+  );
+}
+
+export default Loader;
--- src/services/TodoService.js
+++ src/services/TodoService.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-cycle
 import $api from '../http/api';
 
 export const TodoService = {
