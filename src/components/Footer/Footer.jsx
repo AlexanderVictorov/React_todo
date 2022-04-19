949db4290827aca3eb33ec107d98c918043cf63d
AlexanderVictorov <0676171781.a@gmail.com>
2022-04-19 10:38:16 +0300

    fixStyled[Footer][Header]

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,32 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Box } from '@mui/material';
+import Moment from 'react-moment';
 import Animation from '../Animation/animation';
 
-const StyledBox = styled(Box)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 70px;
-  overflow: hidden;
-  z-index: 1;
-`;
+const styles = {
+  Footer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c)',
+    position: 'fixed',
+    bottom: '0px',
+    left: '0px',
+    width: '100%',
+    height: '70px',
+    overflow: 'hidden',
+    zIndex: 1,
+    fontSize: '20px',
+  },
+};
 
 function Footer() {
   return (
-    <StyledBox>
-      <Box>
-        <Typography variant='h5'>Footer</Typography>
-      </Box>
+    <Box sx={styles.Footer}>
+      <Box />
+      <Moment format='HH:mm' interval={1000} />
       <Animation />
-    </StyledBox>
-
+    </Box>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,33 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import Navigation from '../../pages/Navigation/Navigation';
 import Animation from '../Animation/animation';
 
-const StyledBox = styled(Box)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  background: linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c);
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 70px;
-  overflow: hidden;
-`;
+const styles = {
+  Header: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background: 'linear-gradient(0.25turn, #3f87a6, #ebf8e1, #f69d3c)',
+    position: 'fixed',
+    top: '0px',
+    left: '0px',
+    width: '100%',
+    height: '70px',
+    overflow: 'hidden',
+    zIndex: 1,
+  },
+};
 
 function Header() {
   return (
-    <StyledBox>
+    <Box sx={styles.Header}>
       <Box>
         <Typography variant='h5'>Todos App</Typography>
         <Navigation />
       </Box>
       <Animation />
-    </StyledBox>
-
+    </Box>
   );
 }
 
